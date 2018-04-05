4dfaa4a6751cbc5354aeb7686acb6c8f49079989
IDeepspace <cxin1427@qq.com>
2018-04-05 17:12:06 +0800

    完成作业

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -5,15 +5,47 @@ import java.util.List;
 
 
 public class FizzBuzzGame {
-    private List<String> results = new ArrayList<>();
 
-    public void start(int count) {
+  private List<String> results = new ArrayList<>();
 
+  public void start(int count) {
+    for (int i = 1; i <= count; i++) {
+      String result = play(i);
+      results.add(result);
     }
+  }
 
-    public List<String> getResults() {
-        return results;
+  public List<String> getResults() {
+    return results;
+  }
+
+  public String play(int num) {
+    String numStr = "" + num;
+
+    if (numStr.contains("3")) {
+      numStr = "Fizz";
+    } else {
+      if (num % 3 == 0) {
+        numStr = "Fizz";
+        if (num % 5 == 0) {
+          numStr += "Buzz";
+          if (num % 7 == 0) {
+            numStr += "Whizz";
+          }
+        } else if (num % 7 == 0) {
+          numStr += "Whizz";
+        }
+      } else if (num % 5 == 0) {
+        numStr = "Buzz";
+        if (num % 7 == 0) {
+          numStr += "Whizz";
+        }
+      } else if (num % 7 == 0) {
+        numStr = "Whizz";
+      }
     }
+    return numStr;
+  }
 }
 
 
